a6be3bcd19966e985ead9bd195bf0de991fec2aa
Roald Simonsen <wxd401@alumni.ku.dk>
2021-03-14 17:11:56 +0100

    r

--- tcp-client.hs
+++ tcp-client.hs
@@ -3,12 +3,17 @@
 import Network
 import System.IO
 
+
 main :: IO ()
 main = withSocketsDo $ do
+  -- putStrLn "Address to connect to:"
+  -- serverIP <- getLine
+  -- handle <- connectTo serverIP (PortNumber 3001)
   handle <- connectTo "127.0.0.1" (PortNumber 3001)
-  -- hPutStr handle "Hello, world!"
-  -- newCmd :: String
-  -- newCmd <- readLn
+  putStrLn "Connected!"
   newCmd <- getLine
+  -- let newCmd = "test"
   hPutStr handle newCmd
+  -- response <- hGetLine handle
+  -- putStrLn response
   hClose handle
