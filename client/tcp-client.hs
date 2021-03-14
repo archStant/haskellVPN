cdc60c42b4669c4db7fa74c81e0beff2a07e5688
Roald Simonsen <wxd401@alumni.ku.dk>
2021-03-14 19:58:42 +0100

    Many changes, much wow

--- client/tcp-client.hs
+++ client/tcp-client.hs
@@ -11,9 +11,28 @@ main = withSocketsDo $ do
   -- handle <- connectTo serverIP (PortNumber 3001)
   handle <- connectTo "127.0.0.1" (PortNumber 3001)
   putStrLn "Connected!"
-  newCmd <- getLine
-  -- let newCmd = "test"
-  hPutStr handle newCmd
-  -- response <- hGetLine handle
-  -- putStrLn response
+  hPutStr handle ("initSRProtocol\n")
+  doStuff handle
   hClose handle
+
+doStuff :: Handle -> IO ()
+doStuff handle = do
+  -- Få en linie fra brugeren
+  newCmd <- getLine
+  putStrLn "c1"
+  case newCmd of
+    "exit" -> do
+      hPutStr handle ("exitSRProtocol\n")
+      putStrLn "Closing..."
+    _ -> do
+      -- Send den til serveren
+      hPutStr handle (newCmd ++ "\n")
+      putStrLn "c2"
+      -- Få en linie fra serveren
+      output <- hGetLine handle
+      putStrLn "c3"
+      -- Send den til stdout
+      -- putStrLn output
+      putStrLn "c4"
+      -- Gør det igen
+      doStuff handle
--- server/tcp-server.hs
+++ server/tcp-server.hs
@@ -2,6 +2,7 @@
 import Network
 import System.IO
 import System.Process
+import Control.Concurrent
 
 main :: IO ()
 main = withSocketsDo $ do
@@ -12,11 +13,48 @@ main = withSocketsDo $ do
 
 handleConnections :: Socket -> IO ()
 handleConnections sock = do
+  -- Accept connection from client
   (handle, host, port) <- accept sock
+  -- Check for start of protocol transmission
   output <- hGetLine handle
-  putStrLn output
-  res <- System.Process.readProcess output [] ""
-  -- hPutStr handle (System.Process.readProcess output [] "")
-  putStrLn res
-  -- hPutStr handle "Ok"
+  case output of
+    "initSRProtocol" -> do
+      putStrLn "a"
+      let cp = (proc "/bin/bash" [] )
+               {std_in = CreatePipe, std_out = CreatePipe}
+      putStrLn "b"
+      (Just bashIn, Just bashOut, _, _) <- createProcess cp
+      putStrLn "c"
+      forkIO $ dumpHandle bashOut
+      doStuff handle bashIn
+      -- TODO : Wait på bash-process
+      return ()
+    _ -> return ()
+  hClose handle
   handleConnections sock
+
+dumpHandle :: Handle -> IO ()
+dumpHandle handle = do
+  tmp <- hGetLine handle
+  putStrLn ("> " ++ tmp)
+  -- putStrLn "> "
+  dumpHandle handle
+
+doStuff :: Handle -> Handle -> IO ()
+doStuff handle bashIn = do
+  -- Få en linie fra klienten
+  putStrLn "0"
+  output <- hGetLine handle
+  putStrLn "1"
+  case output of
+    "exitSRProtocol" -> putStrLn "Closing connection"
+    _ -> do
+      -- Send den til stdout
+      putStrLn "2"
+      putStrLn output
+      hPutStr bashIn (output ++ "\n")
+      -- Send en newline til klienten
+      hPutStr handle "\n"
+      putStrLn "3"
+      -- Gør det igen
+      doStuff handle bashIn
