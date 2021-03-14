be9772860aaaad4d227222cce4657aaf1c0ca32e
Roald Simonsen <wxd401@alumni.ku.dk>
2021-03-14 17:46:09 +0100

    Sets up structure and cabal

--- server/tcp-server.hs
+++ server/tcp-server.hs
@@ -1,16 +1,13 @@
 -- tcp-server.hs
 import Network
 import System.IO
-import qualified System.Process
+import System.Process
 
 main :: IO ()
 main = withSocketsDo $ do
   sock <- Network.listenOn $ PortNumber 3001
   putStrLn "Begynder server"
   handleConnections sock
-
-  (mb_stdin_hdl, mb_stdout_hdl, mb_stderr_hdl, ph) <-
-    createProcess (proc "ls" []){ std_out = CreatePipe }
 
 
 handleConnections :: Socket -> IO ()
